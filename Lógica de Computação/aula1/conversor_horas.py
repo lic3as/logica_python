ba3374a5a180589adf8461097527945d3ae29029
Alice Aquino <53798258+lic3as@users.noreply.github.com>
2024-10-04 08:10:15 -0300

    aula1

---
+++ Lógica de Computação/aula1/conversor_horas.py
@@ -0,0 +1,10 @@
+# informar a quantidade de horas
+# converter horas pra segundos (multiplica por 3600)
+# imprimir a quantidade de segundos
+
+print('CONVERSOR DE HORAS PARA SEGUNDOS')
+hrs = int(input('Informe as horas: '))
+
+seg = hrs * 3600
+
+print(f'{hrs} horas equivalem a {seg} segundos.')
---
+++ Lógica de Computação/aula1/media1.py
@@ -0,0 +1,10 @@
+# informar 3 notas
+# calcular e imprimir a média das notas
+
+print('CALCULADORA DE MÉDIAS')
+nota1 = float(input('Informe a primeira nota: '))
+nota2 = float(input('Informe a segunda nota: '))
+nota3 = float(input('Informe a terceira nota: '))
+
+media = (nota1 + nota2 + nota3)/3
+print(f'A média do aluno é {media}.')
